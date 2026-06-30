72592a5866cf05cd17c72bf6cb017ab877ec6d43
namanshharma <namansharma812@gmail>
2026-07-01 02:08:54 +0530

    Initialization of DB connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,12 @@
 import mongoose from 'mongoose';
-import { DB_NAME } from './constant.js';
+import { DB_NAME } from '../constant.js';
 
 const connectDB = async () => {
-    
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    }
+    catch (err) {
+        console.log('Error while connecting to MongoDB', err);
+        process.exit(1);
+    }
 }
